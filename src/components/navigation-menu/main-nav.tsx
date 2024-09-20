ca94c8991714fb0c160230a044eebc8f03ee20ae
Oswaldo Tedesco <oswaldo.tedesco@gmail.com>
2024-09-20 16:37:15 -0400

    fix deployment

--- src/components/navigation-menu/main-nav.tsx
+++ src/components/navigation-menu/main-nav.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { useTranslations } from "next-intl";
 import {
   DollarSign,
   Heart,
   HelpCircle,
   LogIn,
+  Menu,
   Newspaper,
   Phone,
   Search,
@@ -19,6 +21,13 @@ import { SearchModal } from "~/components/search-modal";
 import Link from "next/link";
 import { BrandLogo, BrandLogoWithText } from "../brand-logo";
 import UserDropdownMenu from "../user-dropdown-menu";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerTrigger,
+} from "../ui/drawer";
 
 const menuOptions = {
   highlightedOptions: [
@@ -63,6 +72,7 @@ export interface MainNavProps {
 
 const MobileMenu = () => {
   const t = useTranslations("components->main-nav");
+  const [open, setOpen] = useState(false);
 
   return (
     <div className="fixed bottom-0 left-0 right-0 border-t border-border bg-background/80 backdrop-blur-md md:hidden">
@@ -85,6 +95,54 @@ const MobileMenu = () => {
             <span className="text-xs">{t("Log in")}</span>
           </div>
         </Link>
+        {/* TODO Remove this component after testing*/}
+        <Drawer open={open} onOpenChange={setOpen}>
+          <DrawerTrigger asChild>
+            <Button
+              variant="ghost"
+              className="relative flex items-center space-x-2 rounded-full p-2 hover:bg-accent"
+            >
+              <Menu className="mb-1 h-6 w-6" />
+            </Button>
+          </DrawerTrigger>
+          <DrawerContent>
+            <div className="mt-4 border-t">
+              {menuOptions.highlightedOptions?.map((option) => (
+                <DrawerClose key={option.label} asChild>
+                  <Link key={option.href} href={option.href}>
+                    <Button
+                      variant="ghost"
+                      className="w-full justify-start"
+                      //   onClick={item.onClick}
+                    >
+                      {option.icon ?? null}
+                      <span>{t(option.label)}</span>
+                    </Button>
+                  </Link>
+                </DrawerClose>
+              ))}
+              {menuOptions.options.map((option) => (
+                <DrawerClose key={option.label} asChild>
+                  <Link key={option.href} href={option.href}>
+                    <Button
+                      variant="ghost"
+                      className="w-full justify-start"
+                      //   onClick={item.onClick}
+                    >
+                      {option.icon ?? null}
+                      <span>{t(option.label)}</span>
+                    </Button>
+                  </Link>
+                </DrawerClose>
+              ))}
+            </div>
+            <DrawerFooter>
+              <DrawerClose asChild>
+                <Button variant="outline">{t("Cancel")}</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
       </div>
     </div>
   );
