25f4b0a9e540e309c983de4c57ea0b4819719bd9
Oswaldo Tedesco <oswaldo.tedesco@gmail.com>
2024-09-23 23:14:38 -0400

    fix env vars

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,5 @@
 export const AUTH_SERVER_API =
-  process.env.NEXT_PUBLIC_AUTH_SERVER_API ??
+  process.env.AUTH_SERVER_API ??
   "http://127.0.0.1:8000/cerberus/v1";
 
 export const GOOGLE_MAPS_API_KEY =
